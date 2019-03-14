784c8bd2ad2371d472bbebbc2dd15e217e808535
idant <idant@LAPTOP-1CBL0P6J>
2019-03-14 17:08:06 +0200

    check

---
+++ AP2ex1/MissionContainer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_1
+{
+    public delegate double FuncsDelegate(double num);
+
+    public class FunctionsContainer
+    {
+        private Dictionary<string, FuncsDelegate> funcsDictionary;
+
+        public FunctionsContainer()
+        {
+            this.funcsDictionary = new Dictionary<string, FuncsDelegate>();
+        }
+
+       public FuncsDelegate this [string funcIndex]
+        {
+            get { return  funcsDictionary[funcIndex]; }
+            set { funcsDictionary[funcIndex] = value; }
+        }
+    }
+}
